36995194612133bdb4b2eda9bb117ed760395136
Lucas Oliveira <114187171+LucasOliveiraaa@users.noreply.github.com>
2023-02-28 13:23:25 -0300

    Update ILC_markdown.js

--- tree/ILC_markdown.js
+++ tree/ILC_markdown.js
@@ -1,6 +1,12 @@
 ~function(w){
-  if(!w["ILC_DEFS__"]){
+  if(!w) return
+  if(Object.keys(w).includes("ILC_DEFS__")){
     w["ILC_DEFS__"] = ()=>{
-      // Def here the functions and return in a object
+      // Def here the functions and return in an object
+      // Please, define here just the final functions. To use a support function, define outside the ILC_DEFS__ function
+      // After test your function and apply here, explain the function in `tree/ILC.md`
+      
+      return {}
     }
+  }
 }(window)
